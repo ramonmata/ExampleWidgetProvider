3b5a872853b5142e258bd7b40a6615c90c039b10
Ramón Mata <ramonmata@users.noreply.github.com>
2023-10-25 14:42:21 -0600

    Adds Factory Helper and Program code

--- ExampleWidgetProvider/Program.cs
+++ ExampleWidgetProvider/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program.cs
+
+using System.Runtime.InteropServices;
+using ComTypes = System.Runtime.InteropServices.ComTypes;
+using Microsoft.Windows.Widgets;
+using ExampleWidgetProvider;
+using COM;
+using System;
+
+[DllImport("kernel32.dll")]
+static extern IntPtr GetConsoleWindow();
+
+[DllImport("ole32.dll")]
+
+static extern int CoRegisterClassObject(
+            [MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
+            [MarshalAs(UnmanagedType.IUnknown)] object pUnk,
+            uint dwClsContext,
+            uint flags,
+            out uint lpdwRegister);
+
+[DllImport("ole32.dll")] static extern int CoRevokeClassObject(uint dwRegister);
+
+Console.WriteLine("Registering Widget Provider");
+uint cookie;
+
+Guid CLSID_Factory = Guid.Parse("9F9FFE4D-6C62-49B0-936B-E25D4B486288");
+CoRegisterClassObject(CLSID_Factory, new WidgetProviderFactory<WidgetProvider>(), 0x4, 0x1, out cookie);
+Console.WriteLine("Registered successfully. Press ENTER to exit.");
+Console.ReadLine();
+
+if (GetConsoleWindow() != IntPtr.Zero)
+{
+    Console.WriteLine("Registered successfully. Press ENTER to exit.");
+    Console.ReadLine();
+}
+else
+{
+    // Wait until the manager has disposed of the last widget provider.
+    using (var emptyWidgetListEvent = WidgetProvider.GetEmptyWidgetListEvent())
+    {
+        emptyWidgetListEvent.WaitOne();
+    }
+
+    CoRevokeClassObject(cookie);
+}
